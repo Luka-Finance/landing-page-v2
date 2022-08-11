bfffa07211a34c1b19d221fa20f7f82d1aaf39d8
JayB <ajiboyeabiodun001@gmail.com>
2022-08-12 00:33:53 +0200

    New commit

--- src/components/home/financialFreedomSection/index.jsx
+++ src/components/home/financialFreedomSection/index.jsx
@@ -12,18 +12,19 @@ const FinancialFreedomSection = () => {
           maxwidth="1180px"
           width="100%"
           margin="130px auto 0"
+          mdmargin="70px auto 0"
           mdwidth="90%"
         >
           <TitleText
             size="50px"
-            mdsize="25px"
+            mdsize="20px"
             lineheight="60px"
             color={primaryColor}
             heavy
             textalign="center"
             padding="0 50px"
             mdpadding="0"
-            mdlineheight="35px"
+            mdlineheight="30px"
             margin="0 0 95px"
             mdmargin="0 0 50px"
           >
