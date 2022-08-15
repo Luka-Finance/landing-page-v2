177e2cdaa348363bb0fa40d7fbf68f6e07d5a31f
JayB <ajiboyeabiodun001@gmail.com>
2022-08-15 09:52:14 +0200

    New commit

---
+++ src/pages/BlogPage.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Layout from "../components/layout";
+
+const BlogPage = () => {
+  return (
+    <>
+      <Layout></Layout>
+    </>
+  );
+};
+
+export default BlogPage;
