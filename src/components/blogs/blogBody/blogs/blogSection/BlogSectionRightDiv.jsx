36a766aaba6e9c602aeb56663cc08409376caf02
JayB <ajiboyeabiodun001@gmail.com>
2022-08-29 12:48:42 +0200

    new commit

--- src/components/blogs/blogBody/blogs/blogSection/BlogSectionRightDiv.jsx
+++ src/components/blogs/blogBody/blogs/blogSection/BlogSectionRightDiv.jsx
@@ -5,7 +5,7 @@ const BlogSectionRightDiv = (props) => {
   const { imageUrl, title } = props;
   return (
     <>
-      <Card width="100%" flex="3">
+      <Card width="100%" flex="3" mdradius="10px" overflow="hidden">
         <Image src={imageUrl} alt={title} width="100%" />
       </Card>
     </>
