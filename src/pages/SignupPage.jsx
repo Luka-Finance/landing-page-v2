893f014ce8bf9feefed73ef6a87f9f8f0d42ef64
JayB <ajiboyeabiodun001@gmail.com>
2022-10-01 21:52:19 +0200

    updates

--- src/pages/SignupPage.jsx
+++ src/pages/SignupPage.jsx
@@ -1,7 +1,6 @@
 import React from "react";
 import { Helmet } from "react-helmet";
-import { Card } from "../components/ui";
-import Background from "../components/ui/reusable/Background";
+import Signup from "../components/signup";
 
 const SignupPage = () => {
   return (
@@ -9,20 +8,7 @@ const SignupPage = () => {
       <Helmet>
         <title>Signup || Luka finance</title>
       </Helmet>
-      <Card height="100vh" width="100%" position="fixed" overflowy="hidden">
-        <Background />
-        <Card
-          height="100vh"
-          position="fixed"
-          width="100%"
-          display="flex"
-          alignitems="center"
-          justifycontent="center"
-          overflowy="hidden"
-        >
-          Signup Page
-        </Card>
-      </Card>
+      <Signup />
     </>
   );
 };
